7637b281312e3365f74f97cff85b6dd051d443eb
Songpenga <iiuuyy6654@gmail.com>
2023-04-03 00:53:34 +0900

    Import API js 시도중...

--- src/main/resources/static/js/resv_register.js
+++ src/main/resources/static/js/resv_register.js
@@ -1,6 +1,35 @@
-window.onload = () =>{
+window.onload  = () => {
   ComponentEvent.getInstance().addClickEventRegisterButton();
+}
 
+function requestPay() {
+  IMP.init("imp54127217"); //iamport 대신 자신의 "가맹점 식별코드"를 사용
+  IMP.request_pay(
+    {
+      pg: "html5_inicis",
+      pay_method: "card",
+      merchant_uid: "merchant_" + new Date().getTime(),
+      name: "결제테스트",
+      amount: 100,
+      buyer_email: resvObj.email,
+      buyer_name: resvObj.customerName,
+      buyer_tel: resvObj.number,
+
+    }, function (rsp) {
+      console.log(rsp);
+      ComponentEvent.getInstance().addClickEventRegisterButton();
+      if (rsp.success) {
+        var msg = "결제가 완료되었습니다.";
+        alert(msg);
+        location.href ="http://localhost:8000/menulist"
+        
+      } else {
+        var msg = "결제에 실패하였습니다.";
+        msg += "에러내용 : " + rsp.error_msg;
+        alert(msg);
+      }
+    }
+  );
 }
 
 const resvObj = {
