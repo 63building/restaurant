1baedcaad7608b0649f268b8f6357270342855d5
iiuuyy6654@gmail.com <iiuuyy6654@gmail.com>
2023-05-27 08:05:04 +0900

    Clean Up

--- src/main/java/com/koreait/restaurant/service/MenuListService.java
+++ src/main/java/com/koreait/restaurant/service/MenuListService.java
@@ -1,9 +1,8 @@
 package com.koreait.restaurant.service;
 
-import com.koreait.restaurant.web.dto.MenuListDto;
 import com.koreait.restaurant.repository.MenuRepository;
+import com.koreait.restaurant.web.dto.MenuListDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,7 +10,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class MenuListService {
-    @Autowired
+//    @Autowired
     private final MenuRepository menuRepository;
 
     public List<MenuListDto> getMenuService(MenuListDto menuListDto) {
