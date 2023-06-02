62953d55709af674e87ef17bc3e182bf9053cbb4
DESKTOP-NUOPUIF\ITPS <dndpgml@naver.com>
2023-06-02 11:20:45 +0900

    admin_menu db 수정 예약 조회 db 연결 성공 menu_management page db 연결 성공(이미지빼고) left_aside.js 삭제 menu_management page 이미지 업로드 이미지로 들고 오도록 시도중

--- src/main/java/com/koreait/restaurant/config/SecurityConfig.java
+++ src/main/java/com/koreait/restaurant/config/SecurityConfig.java
@@ -12,9 +12,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http       //other configure params.
+                .csrf().disable();
     }
-
 
     @Bean
     @Override
